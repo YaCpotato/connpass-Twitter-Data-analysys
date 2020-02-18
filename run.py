9668fce0d1c1a1272857457a28d2453fd68f996c
YaCpotato <yasshisshy@gmail.com>
2020-02-19 00:27:04 +0900

    returnの場所を間違えていた

--- run.py
+++ run.py
@@ -2,24 +2,41 @@
 import json, config
 from requests_oauthlib import OAuth1Session
 
+def myTimeLine():
+	url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+	params ={'count' : 50}
+	req = twitter.get(url, params = params)
+	if req.status_code == 200:
+		timeline = json.loads(req.text)
+		for tweet in timeline:
+			print(tweet['user']['name']+'::'+tweet['text'])
+			print(tweet['created_at'])
+			print('----------------------------------------------------')
+	else:
+		print("ERROR: %d" % req.status_code)
+		
+	return
+
 def searchWords():
-	url = "https://api.twitter.com/1.1/search/tweets.json"
+	#url = "https://api.twitter.com/1.1/search/tweets.json"
+	url = "https://api.twitter.com/1.1/tweets/search/30day/MyPortfolio.json"
 	print("何を調べますか?")
 	keyword = input('>> ')
 	print('----------------------------------------------------')
-	params = {'q' : keyword, 'count' : 5}
+	params = {'query' : keyword, 'maxResults' : 10,'fromDate':'202002010000','toDate':'202002100000'}
 	req = twitter.get(url, params = params)
 
 	if req.status_code == 200:
 		search_timeline = json.loads(req.text)
-		for tweet in search_timeline['statuses']:
+		print(len(search_timeline['results']))
+		for tweet in search_timeline['results']:
 			print(tweet['user']['name'] + '::' + tweet['text'])
 			print(tweet['created_at'])
 			print('----------------------------------------------------')
-			return;
-		else:
-			print("ERROR: %d" % req.status_code)
-			return;
+		return;
+	else:
+		print("ERROR: %d" % req.status_code)
+	return;
 
 if __name__ == '__main__':
 	CK = config.CONSUMER_KEY
@@ -28,3 +45,4 @@ if __name__ == '__main__':
 	ATS = config.ACCESS_TOKEN_SECRET
 	twitter = OAuth1Session(CK, CS, AT, ATS)
 	searchWords()
+	#myTimeLine()
