fb4b05b1766f760cf69b7f07b1d7ab25d117d310
YaCpotato <yasshisshy@gmail.com>
2020-02-21 23:52:58 +0900

    data取得

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from requests_oauthlib import OAuth1Session
 
 def myTimeLine():
 	url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-	params ={'count' : 50}
+	params ={'count' : 20}
 	req = twitter.get(url, params = params)
 	if req.status_code == 200:
 		timeline = json.loads(req.text)
@@ -16,6 +16,32 @@ def myTimeLine():
 		print("ERROR: %d" % req.status_code)
 		
 	return
+
+def myTimeLineTweetsId():
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+    params ={'count' : 20}
+    req = twitter.get(url, params = params)
+    ids = []
+    if req.status_code == 200:
+        timeline = json.loads(req.text)
+        for tweet in timeline:
+            ids.append(tweet['id'])
+    else:
+        print("ERROR: %d" % req.status_code)
+ 
+    return ids
+
+def getInpressions():
+    url = "https://data-api.twitter.com/insights/engagement/totals"
+    params = { 'tweet_ids':["1230059041730154497"],"engagement_types": ["impressions","engagements","favorites"],\
+    "groupings": {"grouping name": {"group_by": ["tweet.id"]}}}
+    req = twitter.post(url, params = params)
+    print(vars(req))
+    if req.status_code == 200:
+        print('success')
+    else:
+        print(req.status_code)
+    return
 
 def searchWords():
 	#url = "https://api.twitter.com/1.1/search/tweets.json"
@@ -39,10 +65,11 @@ def searchWords():
 	return;
 
 if __name__ == '__main__':
-	CK = config.CONSUMER_KEY
-	CS = config.CONSUMER_SECRET
-	AT = config.ACCESS_TOKEN
-	ATS = config.ACCESS_TOKEN_SECRET
-	twitter = OAuth1Session(CK, CS, AT, ATS)
-	searchWords()
-	#myTimeLine()
+    CK = config.CONSUMER_KEY
+    CS = config.CONSUMER_SECRET
+    AT = config.ACCESS_TOKEN
+    ATS = config.ACCESS_TOKEN_SECRET
+    twitter = OAuth1Session(CK, CS, AT, ATS)
+    #searchWords()
+    #myTimeLineTweetsId()
+    getInpressions()
