cef813fa7cb88f3562b4468ffab68aaef8683769
YaCpotato <yasshisshy@gmail.com>
2020-03-01 13:29:53 +0900

    インデント調整

--- dashboard.py
+++ dashboard.py
@@ -31,72 +31,75 @@ events_date = [
 subject = ['インプレッション数','RT数','いいね数']
 by_tweet_or_date = ['ツイート毎','日付毎']
 
-app.layout = html.Div(children=[
-    html.Div(
-        id='state-value',
-        style={'display': 'none'},
-        children=[]),
-    html.Div(
-        style={ 'width':'100vw',
-                'minHeight':'15vh',
-                'margin':'0 auto',
-                'textAlign':'center'
-                },
-        children=[
-        html.Span(
-            style={'margin':'16px','fontSize':'28px','fontWeight':'bold'},
-            children='Machine Learning for Beginners! Twitter分析ダッシュボード'),
-        ]
-    ),
-    html.Div(
-        style={ 'width':'100vw',
-                'minHeight':'15vh',
-                'margin':'0 auto',
-                'textAlign':'left',
-                'display':'flex'
-                },
-        children=[
-        dcc.Dropdown(
-            id='event-number',
-            style={'width':'10vw','marginRight':'8px'},
-            options=[{'label': '第'+str(i + 1)+'回', 'value': i} for i in events_number],
-            value=events_number[0]
-        ),
-        dcc.Dropdown(
-                id='subject',
-                style={'width':'15vw','marginRight':'8px'},
-                options=[{'label': i, 'value': i} for i in subject],
-                value=subject[0]
-            ),
-        ]
-    ),
-    html.Div(
-        style={'display':'flex'},
-        children=[
-        dcc.Dropdown(
-            id='grain',
-            style={'width':'10vw','marginRight':'8px'},
-            options=[{'label': i, 'value': i} for i in by_tweet_or_date],
-            value=by_tweet_or_date[0]
-        )
-    ]
-),
-    html.Div(
-        style={'display':'flex'},
-        children=[
+app.layout = html.Div(
+    children=[
         html.Div(
-            style={'width':'50vw'},
+            id='state-value',
+            style={'display': 'none'},
+            children=[]),
+        html.Div(
+            style={ 'width':'100vw',
+                    'minHeight':'15vh',
+                    'margin':'0 auto',
+                    'textAlign':'center'
+                    },
             children=[
-            dcc.Graph(id='graph-with-dropdown')   
+                html.Span(
+                    style={'margin':'16px','fontSize':'28px','fontWeight':'bold'},
+                    children='Machine Learning for Beginners! Twitter分析ダッシュボード'),
             ]
         ),
         html.Div(
-            id='tweet-list',
-            style={'width':'50vw','maxHeight':'80vh','overflowY':'scroll'},
-            children=[]
+            style={ 'width':'100vw',
+                    'minHeight':'15vh',
+                    'margin':'0 auto',
+                    'textAlign':'left',
+                    'display':'flex'
+                    },
+            children=[
+            dcc.Dropdown(
+                id='event-number',
+                style={'width':'10vw','marginRight':'8px'},
+                options=[{'label': '第'+str(i + 1)+'回', 'value': i} for i in events_number],
+                value=events_number[0]
+            ),
+            dcc.Dropdown(
+                    id='subject',
+                    style={'width':'15vw','marginRight':'8px'},
+                    options=[{'label': i, 'value': i} for i in subject],
+                    value=subject[0]
+                ),
+            ]
+        ),
+        html.Div(
+            style={'display':'flex'},
+            children=[
+                dcc.Dropdown(
+                    id='grain',
+                    style={'width':'10vw','marginRight':'8px'},
+                    options=[{'label': i, 'value': i} for i in by_tweet_or_date],
+                    value=by_tweet_or_date[0]
+                )
+            ]
+        ),
+        html.Div(
+            style={'display':'flex'},
+            children=[
+                html.Div(
+                    style={'width':'50vw'},
+                    children=[
+                    dcc.Graph(id='graph-with-dropdown')   
+                    ]
+                ),
+                html.Div(
+                    id='tweet-list',
+                    style={'width':'50vw','maxHeight':'80vh','overflowY':'scroll'},
+                    children=[]
+                )
+            ]
         )
     ]
-)])
+)
 
 @app.callback(
     Output('state-value','children'),
@@ -176,128 +179,6 @@ def update_display_event(data):
             title = subject
         )
     },generate_table(tweet_df)
-
-"""
-@app.callback(
-    Output('graph-with-dropdown', 'figure'),
-    [Input('subject','value'),Input('event-number','value')])
-def update_display_event(subject,event_number):
-    tweets = session.query(Tweet.content,Tweet.date,Tweet.inpression,Tweet.retweet,Tweet.like).\
-    filter(
-        Tweet.date >= datetime(events_date[int(event_number)][0],events_date[int(event_number)][1],events_date[int(event_number)][2]),
-        Tweet.date < datetime(events_date[int(event_number)][3],events_date[int(event_number)][4],events_date[int(event_number)][5]),
-    ).\
-    distinct(Tweet.date).all()
-
-    shaped_tweets = shape_tweets_by_day(tweets)
-
-    ml_date = []
-    ml_content = []
-    ml_subject = []
-    for twi in shaped_tweets:
-        ml_date.append(twi["date"])
-        ml_content.append(twi["content"])
-        if (subject == 'インプレッション数'):
-            ml_subject.append(twi["impressions"])
-        elif (subject == 'RT数'):
-            ml_subject.append(twi["retweets"])
-        elif (subject == 'いいね数'):
-            ml_subject.append(twi["likes"])
-        ml_retweet.append(twi["retweets"])
-        ml_like.append(twi["likes"])
-
-    traces = []
-    traces.append(dict(
-        x = ml_date,
-        y = ml_subject,
-        type = 'bar'
-    ))
-
-    return {
-        'data': traces,
-        'layout': dict(
-            title = subject
-        )
-    }
-
-@app.callback(
-    Output('graph-with-dropdown', 'figure'),
-    [Input('grain','value')])
-def update_grain(subject,event_number):
-    tweets = session.query(Tweet.content,Tweet.date,Tweet.inpression,Tweet.retweet,Tweet.like).\
-    filter(
-        Tweet.date >= datetime(events_date[int(event_number)][0],events_date[int(event_number)][1],events_date[int(event_number)][2]),
-        Tweet.date < datetime(events_date[int(event_number)][3],events_date[int(event_number)][4],events_date[int(event_number)][5]),
-    ).\
-    distinct(Tweet.date).all()
-
-    shaped_tweets = shape_tweets_by_day(tweets)
-
-    ml_date = []
-    ml_content = []
-    ml_subject = []
-    for twi in shaped_tweets:
-        ml_date.append(twi["date"])
-        ml_content.append(twi["content"])
-        if (subject == 'インプレッション数'):
-            ml_subject.append(twi["impressions"])
-        elif (subject == 'RT数'):
-            ml_subject.append(twi["retweets"])
-        elif (subject == 'いいね数'):
-            ml_subject.append(twi["likes"])
-    
-
-    traces = []
-    traces.append(dict(
-        x = ml_date,
-        y = ml_subject,
-        type = 'bar'
-    ))
-
-    return {
-        'data': traces,
-        'layout': dict(
-            title = subject
-        )
-    }
-
-
-@app.callback(
-    Output('tweet-list', 'children'),
-    [Input('event-number','value')])
-def update_table_event(event_number):
-    tweets = session.query(Tweet.content,Tweet.date,Tweet.inpression,Tweet.retweet,Tweet.like).\
-    filter(
-        Tweet.date >= datetime(events_date[int(event_number)][0],events_date[int(event_number)][1],events_date[int(event_number)][2]),
-        Tweet.date < datetime(events_date[int(event_number)][3],events_date[int(event_number)][4],events_date[int(event_number)][5]),
-    ).\
-    distinct(Tweet.date).all()
-
-    shaped_tweets = shape_tweets_by_day(tweets)
-
-    ml_date = []
-    ml_content = []
-    ml_impression = []
-    ml_retweet = []
-    ml_like = []
-    for twi in shaped_tweets:
-        ml_date.append(twi["date"])
-        ml_content.append(twi["content"])
-        ml_impression.append(twi["impressions"])
-        ml_retweet.append(twi["retweets"])
-        ml_like.append(twi["likes"])
-    
-    tweet_df = pd.DataFrame({
-        'date' : ml_date,
-        'content' : ml_content,
-        'impression' : ml_impression,
-        'retweet' : ml_retweet,
-        'like' : ml_like
-    })
-
-    return generate_table(tweet_df)
-"""
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
