bce0af8be524544e5928e924d59e19f62dc4dd74
YaCpotato <yasshisshy@gmail.com>
2020-03-05 00:47:52 +0900

    ログインテンプレート追加。動作未確認

---
+++ manage_frask/app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+from .auth import auth as auth_blueprint
+from .main import main as main_blueprint
+
+
+def create_app():
+    app = Flask(__name__)
+    #app.config.from_object('application.config.Config')
+    #initialize_database(app)
+
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+
+app = create_app()
