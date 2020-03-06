d54a27f3001c096f9b0cae6d421509a016d8976e
YaCpotato <yasshisshy@gmail.com>
2020-03-06 21:42:19 +0900

    models.py統合

--- manage_frask/app/models/models.py
+++ manage_frask/app/models/models.py
@@ -26,3 +26,21 @@ class User(db.Model, UserMixin): # UserMixin を追記
         self.display_name = display_name
         self.email = email
         self.password = password
+    
+class Tweet(db.Model):
+	"""
+	TweetModel
+	"""
+	__tablename__ = 'tweets'
+	tweet_id = Column(BigInteger, primary_key=True)
+	content = Column(Text)
+	date = Column(DateTime, default=datetime.now, nullable=True)
+	inpression = Column(Integer,nullable=True)
+	engagement = Column(Integer,nullable=True)
+	retweet = Column(Integer,nullable=True)
+	reply = Column(Integer,nullable=True)
+	like = Column(Integer,nullable=True)
+	profile_click = Column(Integer,nullable=True)
+	url_click = Column(Integer,nullable=True)
+	hashtag_click = Column(Integer,nullable=True)
+	detail_click = Column(Integer,nullable=True)
